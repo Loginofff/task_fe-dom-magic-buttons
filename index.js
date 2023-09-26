d94728798c3d755c347978ed90d35ee8f8b45ed2
Dmitrijs Loginovs <132077317+Loginofff@users.noreply.github.com>
2023-09-26 20:41:08 +0200

    homeWork

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 // найдем первую кнопку по id
 const btn = document.getElementById("magic-btn");
+console.log(btn);
+
 // скопируем ее - получилась вторая кнопка. Ее можно использовать.
 const clonedBtn = btn.cloneNode(true);
 // задали id
+
 clonedBtn.id = "magic-btn-2";
 
+
 btn.addEventListener('click', () => {
+  
+  console.log("click");
   // при нажатии на первую кнопку
   // работайте с clonedBtn
-  // ваш код начинается здесь
-  
+  console.log(clonedBtn);
+  document.body.appendChild(clonedBtn);
+  clonedBtn.style.color = "white";
+  clonedBtn.style.borderRadius = "15px";
+  clonedBtn.innerText = "Я изменю тебя";
+  clonedBtn.style.background = "#a78b71";
 })
 
-// здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click' , () => {
+  btn.style.background = "black";
+  btn.style.color = "white";
+})
